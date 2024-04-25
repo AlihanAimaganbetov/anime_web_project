5029f3ab2eee397a7896ef6ef5a77cd86f7dceb5
AlihanAimaganbetov <126770656+AlihanAimaganbetov@users.noreply.github.com>
2024-04-25 16:22:13 +0500

    commit

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { addToFavorites } from '../redux/action'; 
+import { addToFavorites } from '../redux/action';
 import { Link } from 'react-router-dom';
 import './App.css'
 
@@ -11,11 +11,10 @@ interface Anime {
     score: number;
     uid: number;
     img_url: string;
-    synopsis:string;
+    synopsis: string;
     aired: string;
-    episodes : number;
+    episodes: number;
 }
-
 const App: React.FC<{}> = () => {
     const [items, setItems] = useState<Anime[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(1);
@@ -45,7 +44,7 @@ const App: React.FC<{}> = () => {
         // Возвращаем год
         return year;
     };
-    
+
     const sortByAired = () => {
         const sortedItems = [...items].sort((a, b) => {
             const yearA = extractYear(a.aired);
@@ -54,20 +53,18 @@ const App: React.FC<{}> = () => {
             return yearB - yearA;
         });
         setItems(sortedItems);
-        setCurrentPage(1); 
+        setCurrentPage(1);
     };
-    
+
     useEffect(() => {
         const filteredResults = items.filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()));
         setTotalPages(Math.ceil(filteredResults.length / 10));
         setCurrentPage(1);
     }, [searchQuery, items]);
-
     const addToFavoritesHandler = (item: Anime) => {
         dispatch(addToFavorites(item));
-        alert("добавлено")
+        alert("Add")
     };
-
     useEffect(() => {
         const filteredResults = items.filter(item => selectedGenre ? item.genre.toLowerCase().includes(selectedGenre.toLowerCase()) : true);
         setTotalPages(Math.ceil(filteredResults.length / 10));
@@ -83,7 +80,7 @@ const App: React.FC<{}> = () => {
     const sortByScore = () => {
         const sortedItems = [...items].sort((a, b) => b.score - a.score);
         setItems(sortedItems);
-        setCurrentPage(1); 
+        setCurrentPage(1);
     };
 
     const handlePrevPage = () => {
@@ -100,9 +97,9 @@ const App: React.FC<{}> = () => {
     const endIndex = currentPage * 10;
 
     const pageButtons: JSX.Element[] = [];
-    const maxButtonsToShow: number = 10; 
+    const maxButtonsToShow: number = 10;
 
-    
+
     let startPage = Math.max(1, currentPage - Math.floor(maxButtonsToShow / 2));
     let endPage = Math.min(totalPages, startPage + maxButtonsToShow - 1);
     if (endPage === totalPages) {
@@ -118,66 +115,66 @@ const App: React.FC<{}> = () => {
         );
     }
     return (<div className="app-container">
-    <header className="app-header">
-        <Link to="favorites">
-            <button>Favorites</button>
-        </Link>
-        <input
-            className="search-input"
-            type="text"
-            placeholder="Search by title..."
-            value={searchQuery}
-            onChange={e => setSearchQuery(e.target.value)}
-        />
-        <button onClick={sortByScore}>Sort by Score</button>
-        <button onClick={sortByAired}>Sort by Aired</button>
-        <label htmlFor="genres">Select Genre:</label>
-        <select id="genres" onChange={e => setSelectedGenre(e.target.value)}>
-            <option value="">All Genres</option>
-            {genre.map(genre => (
-                <option key={genre} value={genre}>
-                    {genre}
-                </option>
-            ))}
-        </select>
-    </header>
-    <ul>
-        {items
-            .filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()))
-            .filter(item => selectedGenre ? item.genre.toLowerCase().includes(selectedGenre.toLowerCase()) : true)
-            .slice(startIndex, endIndex)
-            .map((item, index) => (<li key={item.uid || index}>
-                <div className='anime'>
-                    <p>{item.title}</p>
-                    <div className='img-synopsis'>
-                        <div className='app-img'>
-                            <Link to={`/anime/${item.uid}`}>
-                                <img src={item.img_url} alt={item.title} />
-                            </Link>
-                        </div>
-                        <div className='app-synopsis'>
-                            <p><b>Description: </b> {item.synopsis}</p>
-                            <p><b>Score: </b> {item.score}</p>
-                            <p><b>Episodes: </b> {item.episodes}</p>
-                            <p><b>Genre: </b>{item.genre}</p>
-                            <p><b>Aired: </b>{item.aired}</p>
+        <header className="app-header">
+            <Link to="favorites">
+                <button>Favorites</button>
+            </Link>
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Search by title..."
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+            />
+            <button onClick={sortByScore}>Sort by Score</button>
+            <button onClick={sortByAired}>Sort by Aired</button>
+            <label htmlFor="genres">Select Genre:</label>
+            <select id="genres" onChange={e => setSelectedGenre(e.target.value)}>
+                <option value="">All Genres</option>
+                {genre.map(genre => (
+                    <option key={genre} value={genre}>
+                        {genre}
+                    </option>
+                ))}
+            </select>
+        </header>
+        <ul>
+            {items
+                .filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()))
+                .filter(item => selectedGenre ? item.genre.toLowerCase().includes(selectedGenre.toLowerCase()) : true)
+                .slice(startIndex, endIndex)
+                .map((item, index) => (<li key={item.uid || index}>
+                    <div className='anime'>
+                        <p>{item.title}</p>
+                        <div className='img-synopsis'>
+                            <div className='app-img'>
+                                <Link to={`/anime/${item.uid}`}>
+                                    <img src={item.img_url} alt={item.title} />
+                                </Link>
+                            </div>
+                            <div className='app-synopsis'>
+                                <p><b>Description: </b> {item.synopsis}</p>
+                                <p><b>Score: </b> {item.score}</p>
+                                <p><b>Episodes: </b> {item.episodes}</p>
+                                <p><b>Genre: </b>{item.genre}</p>
+                                <p><b>Aired: </b>{item.aired}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <button onClick={() => addToFavoritesHandler(item)}>Добавить в избранное</button>
-            </li>))}
+                    <button onClick={() => addToFavoritesHandler(item)}>Добавить в избранное</button>
+                </li>))}
 
-    </ul>
+        </ul>
 
-    <div className="pagination">
-        <button onClick={handlePrevPage} disabled={currentPage === 1}>
-            Previous Page
-        </button>
-        {pageButtons}
-        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-            Next Page
-        </button>
-    </div>
-</div>);
+        <div className="pagination">
+            <button onClick={handlePrevPage} disabled={currentPage === 1}>
+                Previous Page
+            </button>
+            {pageButtons}
+            <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                Next Page
+            </button>
+        </div>
+    </div>);
 }
 export default App;
